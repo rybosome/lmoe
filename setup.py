f75547977c336ddf3d2e0523638117e42d074f1d
Ryan Eiger <eiger.ryan@gmail.com>
2024-02-08 00:44:55 -0800

    Fix PyPi doc bug.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from lmoe.framework.expert_registry import get_all_modelfile_names
 
 setup(
     name="lmoe",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     package_data={"lmoe": get_all_modelfile_names()},
 )
