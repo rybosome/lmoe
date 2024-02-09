f128ed92ca5c00921d82881b1f26b97a77dcce1f
Ryan Eiger <eiger.ryan@gmail.com>
2024-02-09 14:36:46 -0800

    Fix whitespace in lmoe_query.

--- lmoe/api/lmoe_query.py
+++ lmoe/api/lmoe_query.py
@@ -18,9 +18,11 @@ $user_query
 """
 )
 
+
 @dataclass
 class LmoeQuery:
     """A query plus the associated STDIN/paste context to be passed to an underlying lmoe model."""
+
     stdin_context: Optional[str]
     paste_context: Optional[str]
     user_query: str
@@ -28,6 +30,7 @@ class LmoeQuery:
     def render(self) -> str:
         """Render all context and the query into a form suitable for sending to an lmoe model."""
         return _PROMPT_TEMPLATE.substitute(
-                stdin_context=self.stdin_context,
-                paste_context=self.paste_context,
-                user_query=self.user_query)
+            stdin_context=self.stdin_context,
+            paste_context=self.paste_context,
+            user_query=self.user_query,
+        )
