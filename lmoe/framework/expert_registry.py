58684a7fd653929513e57fbd23e55392cf6c4016
Ryan Eiger <eiger.ryan@gmail.com>
2024-02-09 14:33:49 -0800

    Add registry for experts.

--- lmoe/framework/expert_registry.py
+++ lmoe/framework/expert_registry.py
@@ -1,26 +1,51 @@
-_REGISTRY = {}
+from injector import Injector, inject
+from typing import List
 
 
-def register_experts(*args):
-    for arg in args:
-        _REGISTRY[arg.name()] = arg
+def expert(cls):
+    """Decorator to register an Expert with the ExpertRegistry."""
+    ExpertRegistry.register(cls)
+    return cls
 
 
-def keys():
-    return _REGISTRY.keys()
+class ExpertRegistry:
+    """A registry of all of the individual experts within lmoe.
 
+    Experts are keyed on their name, which allows the default experts to be overridden with
+    user-defined experts.
+    """
 
-def values():
-    return _REGISTRY.values()
+    _expert_registry = {}
 
+    @inject
+    def __init__(self, injector: Injector):
+        self.injector = injector
 
-def get_all_modelfile_names():
-    return [e.modelfile_name() for e in values() if e.has_modelfile()]
+    @classmethod
+    def register(cls, *args) -> None:
+        """Register one or more experts - does not instantiate them."""
+        for expert_type in args:
+            cls._expert_registry[expert_type.name()] = expert_type
 
+    @classmethod
+    def names(cls) -> List[str]:
+        """List of experts which have been registered."""
+        return [key for key in cls._expert_registry.keys()]
 
-def get_registered_names():
-    return [name for name in _REGISTRY.keys()]
+    @classmethod
+    def modelfile_names(cls) -> List[str]:
+        """List of known modelfiles backed by registered experts."""
+        return [
+            expert_type.modelfile_name()
+            for expert_type in cls._expert_registry.values()
+            if expert_type.has_modelfile()
+        ]
 
+    def experts(self):
+        return [self.get(expert_name) for expert_name in ExpertRegistry.names()]
 
-def get_expert(expert_name):
-    return _REGISTRY[expert_name]
+    def get(self, expert_name):
+        if expert_name not in self._expert_registry:
+            return None
+        expert_type = self._expert_registry[expert_name]
+        return self.injector.get(expert_type)
