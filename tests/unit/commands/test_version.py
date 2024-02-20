1abdebfd6b8164d00fceba9926a03a1ec12dc9a3
Ryan Eiger <eiger.ryan@gmail.com>
2024-02-19 21:16:14 -0800

    Refactored TestVersion to use the util function for console output.

--- tests/unit/commands/test_version.py
+++ tests/unit/commands/test_version.py
@@ -1,19 +1,14 @@
 import argparse
-import contextlib
-import io
 
 from lmoe.api.lmoe_query import LmoeQuery
 from lmoe.commands.version import Version
 from lmoe.definitions import get_project_version
+from tests.util.capture_output import capture_console_output
 
 
 class TestVersion:
 
-    def test_print_version(self):
-        f = io.StringIO()
-        with contextlib.redirect_stdout(f):
-            Version().execute(
-                argparse.Namespace(),
-                LmoeQuery(stdin_context="", paste_context="", user_query=""),
-            )
-        assert f"{get_project_version()}" in f.getvalue()
+    def test_print_version_capture_console_output(self):
+        with capture_console_output() as console_output:
+            Version().execute(argparse.Namespace(), LmoeQuery.empty())
+            assert f"{get_project_version()}" in console_output.getvalue()
