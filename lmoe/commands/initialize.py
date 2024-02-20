d338a7b1d02550f75f6ad13b65fb4da8c8658d4b
Ryan Eiger <eiger.ryan@gmail.com>
2024-02-20 10:04:02 -0800

    Update initalize to use framework components

--- lmoe/commands/initialize.py
+++ lmoe/commands/initialize.py
@@ -8,6 +8,8 @@ from lmoe.api.lmoe_query import LmoeQuery
 from lmoe.commands.command import Command
 from lmoe.experts.refresh import Refresh
 from lmoe.framework.expert_registry import ExpertRegistry
+from lmoe.framework.model_registry import ModelRegistry
+from lmoe.framework.ollama_client import OllamaClient
 
 
 @inject
@@ -15,39 +17,20 @@ from lmoe.framework.expert_registry import ExpertRegistry
 class Initialize(Command):
 
     expert_registry: ExpertRegistry
+    model_registry: ModelRegistry
+    ollama_client: OllamaClient
     refresh: Refresh
 
-    def _get_all_base_models(self):
-        base_models = set()
-        for expert in [e for e in self.expert_registry.experts() if e.has_model()]:
-            modelfile_contents = expert.model().modelfile_contents()
-            match = re.match(r"FROM ([^\s]+).*", modelfile_contents)
-            if match:
-                base_models.add(match.group(1))
-        return base_models
-
-    def _get_installed_models(self):
-        response = ollama.list()
-        existing_model_names = [
-            model["name"].split(":")[0] for model in response["models"]
-        ]
-        return set(existing_model_names)
-
     def execute(self, parsed_args: argparse.Namespace, lmoe_query: LmoeQuery) -> None:
-        base_models_needed = self._get_all_base_models()
+        base_models_needed = self.model_registry.base_models_required()
         print("Base Ollama models needed: ", base_models_needed)
-        installed_models = self._get_installed_models()
+        installed_models = self.model_registry.installed_ollama_model_names()
         for base_model in base_models_needed:
             if base_model in installed_models:
                 print(f"  {base_model} already installed")
             else:
                 print(f"  Pulling {base_model} from Ollama")
-                stream = ollama.pull(base_model, stream=True)
-                last_message = ""
+                stream = self.ollama_client.install_ollama_model(base_model)
                 for chunk in stream:
-                    current_message = chunk["status"]
-                    if current_message != last_message:
-                        print("    ", current_message)
-                    last_message = current_message
-                installed_models.add(base_model)
+                    print("    ", chunk)
         self.refresh.generate(lmoe_query)
