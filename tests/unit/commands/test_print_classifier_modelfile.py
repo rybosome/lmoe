9ff07fc48b45a36a3be3fcd0e3508cc05fa58a97
Ryan Eiger <eiger.ryan@gmail.com>
2024-02-19 21:50:21 -0800

    Add print classifier modelfile test and update version test.

---
+++ tests/unit/commands/test_print_classifier_modelfile.py
@@ -0,0 +1,16 @@
+import argparse
+
+from lmoe.api.lmoe_query import LmoeQuery
+from lmoe.commands.print_classifier_modelfile import PrintClassifierModelfile
+from tests.util.capture_output import capture_console_output
+from tests.util.real_injector import real_injector
+
+
+class TestPrintClassifierModelfile:
+
+    def test_print_classifier_modelfile(self, real_injector):
+        with capture_console_output() as console_output:
+            real_injector.get(PrintClassifierModelfile).execute(
+                argparse.Namespace(), LmoeQuery.empty()
+            )
+            assert "you are a classifier" in console_output.getvalue()
--- tests/unit/commands/test_version.py
+++ tests/unit/commands/test_version.py
@@ -4,11 +4,12 @@ from lmoe.api.lmoe_query import LmoeQuery
 from lmoe.commands.version import Version
 from lmoe.definitions import get_project_version
 from tests.util.capture_output import capture_console_output
+from tests.util.real_injector import real_injector
 
 
 class TestVersion:
 
-    def test_print_version_capture_console_output(self):
+    def test_print_version(self, real_injector):
         with capture_console_output() as console_output:
-            Version().execute(argparse.Namespace(), LmoeQuery.empty())
+            real_injector.get(Version).execute(argparse.Namespace(), LmoeQuery.empty())
             assert f"{get_project_version()}" in console_output.getvalue()
